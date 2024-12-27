f78929a4919678684353c932211e7248fc3570e9
Vaibhavvvvvvvvv <777vsolanki@gmail.com>
2024-12-27 21:49:37 +0530

    nesting_routing

--- gardenRevivalSystem/src/App.jsx
+++ gardenRevivalSystem/src/App.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
-import DashBoard from './DashBoard/DashBoard'; 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+import DashBoard from './DashBoard/DashBoard';
+import ProtectedRoute from './components/protectedRoute';
+
 const App = () => {
   return (
-    <div>
-      <DashBoard />
-    </div>
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <DashBoard />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="*" element={<Login />} />
+        </Routes>
+      </Router>
+    </Provider>
   );
 };
 
---
+++ gardenRevivalSystem/src/pages/Login.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { login } from '../redux/slices/authSlice';
+import { useNavigate } from 'react-router-dom';
+
+const Login = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    dispatch(login());
+    navigate('/dashboard');
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+};
+
+export default Login;
