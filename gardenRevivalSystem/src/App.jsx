adc8145903277dff2b5cb3df391ccb6d56986669
Vaibhavvvvvvvvv <777vsolanki@gmail.com>
2024-12-27 12:22:06 +0530

    initialize React application with Vite setup and basic structure

---
+++ gardenRevivalSystem/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import DashBoard from './Component/DashBoard'; 
+const App = () => {
+  return (
+    <div>
+      <DashBoard />
+    </div>
+  );
+};
+
+export default App;
